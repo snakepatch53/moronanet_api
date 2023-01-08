c07966686fdc6f6ffbe8ecc4a237e081742afd99
snakepatch53 <snakepatch53@gmail.com>
2023-01-08 18:11:11 -0500

    Solucion a servicio de contratos con integracion de QR

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/functions/qrcode_location.php';
 
 $router = new \Bramus\Router\Router();
 
---
+++ src/functions/qrcode_location.php
@@ -0,0 +1,40 @@
+<?php
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+
+
+function qrcode_location($text)
+{
+
+    $result = Builder::create()
+        ->writer(new PngWriter())
+        ->writerOptions([])
+        ->data("https://www.google.com.ec/maps/place/" . str_replace(' ', '', $text))
+        ->encoding(new Encoding('UTF-8'))
+        ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+        ->size(300)
+        ->margin(0)
+        ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+        ->labelText('')
+        ->labelFont(new NotoSans(20))
+        ->labelAlignment(new LabelAlignmentCenter())
+        ->validateResult(false)
+        ->build();
+
+
+    // Directly output the QR code
+    // header('Content-Type: ' . $result->getMimeType());
+    return base64_encode($result->getString());
+
+    // Save it to a file
+    // $result->saveToFile(__DIR__ . '/qrcode.png');
+
+    // Generate a data URI to include image data inline (i.e. inside an <img> tag)
+    // $dataUri = $result->getDataUri();
+}
