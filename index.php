a38e283b95f40199bbda8b755ba5322da92fe8d4
snakepatch53 <snakepatch53@gmail.com>
2023-02-17 19:25:51 -0500

    s

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+//show errors
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
 date_default_timezone_set('America/Guayaquil');
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/functions/qrcode_location.php';
